a2317a029046b03f04d52ac660bdd4537a0491af
Ebinuma Kenichi <ktennis.mqekr12@gmail.com>
2017-08-04 02:24:35 +0900

    complete user function

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to root_url, notice: 'ユーザー情報の更新に成功しました'
+      sign_in(current_user, bypass: true)
+    else
+      render :edit
+    end
+  end
+
+  def show; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :avatar,
+      :occupation,
+      :profile,
+      :position
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: %i[edit update show]
   resources :prototypes, only: %i[index show new]
   root 'prototypes#index'
 end
