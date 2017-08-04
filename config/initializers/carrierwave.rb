7bb9674d3abf18a72ffeb587f2c97c8496406fc3
Ebinuma Kenichi <ktennis.mqekr12@gmail.com>
2017-08-05 02:19:24 +0900

    modify env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,15 +8,15 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1'
+    region: ENV['AWS_REGION']
   }
 
   case Rails.env
   when 'development'
-    config.fog_directory = 'ebkn12-protospace-image'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ebkn12-protospace-image'
+    config.fog_directory = ENV['AWS_S3_BUCKET']
+    config.asset_host = "https://#{ENV['AWS_REGION']}.amazonaws.com/#{ENV['AWS_S3_BUCKET']}"
   when 'production'
-    config.fog_directory = 'ebkn12-protospace-image'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ebkn12-protospace-image'
+    config.fog_directory = ENV['AWS_S3_BUCKET']
+    config.asset_host = "https://#{ENV['AWS_REGION']}.amazonaws.com/#{ENV['AWS_S3_BUCKET']}"
   end
 end
