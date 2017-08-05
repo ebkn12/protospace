abfd85294197e08c88fd651cd005c1c1e352a0bf
Ebinuma Kenichi <ktennis.mqekr12@gmail.com>
2017-08-06 02:00:09 +0900

    set prototype action button

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: %i[edit update show]
-  resources :prototypes, only: %i[index new create show]
+  resources :prototypes
   root 'prototypes#index'
 end
