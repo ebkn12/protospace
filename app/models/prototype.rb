42a187fd4b10bbb13c179cfa77e39fef986c6d35
Ebinuma Kenichi <ktennis.mqekr12@gmail.com>
2017-08-06 14:00:00 +0900

    create update function

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,14 @@
 class Prototype < ApplicationRecord
   belongs_to :user
-  has_many :captured_images
+  has_many :captured_images, dependent: :destroy
   accepts_nested_attributes_for :captured_images, allow_destroy: true
+  validates :title, :user_id, presence: true
+
+  def main_image
+    captured_images.find_by(status: 1)
+  end
+
+  def sub_images
+    captured_images.where(status: 0)
+  end
 end
