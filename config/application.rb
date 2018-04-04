8fddf424af8495d22501521e20ecd86b27118198
ebkn12 <ktennis.mqekr12@gmail.com>
2018-04-04 23:53:11 +0900

    feat: rails app:update

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ Bundler.require(*Rails.groups)
 
 module Protospace
   class Application < Rails::Application
+    config.load_defaults 5.1
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,9 +14,12 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.read_encrypted_secrets = true
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  # config.public_file_server.enabled = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :scss
@@ -77,7 +80,7 @@ Rails.application.configure do
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
