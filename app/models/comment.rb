6d72bd8f075a0eb4af02728619a8a9863267903e
ebkn12 <ktennis.mqekr12@gmail.com>
2018-04-25 20:42:47 +0900

    test: comment model spec

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :prototype
 
-  validates :content, :user_id, :prototype_id, presence: true
+  validates :content, presence: true
 
   paginates_per 10
 end
