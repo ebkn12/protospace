74c78f94c25521c3c9300d106c7708e34ef0ab1d
ebkn12 <ktennis.mqekr12@gmail.com>
2018-04-26 01:38:20 +0900

    test: prototyptes_controller spec

--- spec/controllers/prototypes_controller_spec.rb
+++ spec/controllers/prototypes_controller_spec.rb
@@ -1,181 +1,160 @@
 require 'rails_helper'
 
-describe PrototypesController do
-  let(:user) { subject.current_user }
-  let(:test_user) {
-    create(
-      :user,
-      name: Faker::StarWars.character,
-      email: Faker::Internet.email
-    )
-  }
-  let(:prototype) { create(:prototype, user_id: test_user.id) }
-  let(:sample_title) { Faker::StarWars.character }
-  let(:prototype_params) {{
-    id: prototype.id,
-    prototype: attributes_for(:prototype, title: sample_title)
-  }}
-  let(:invalid_prototype_params) {{
-    id: prototype.id,
-    prototype: attributes_for(:prototype, title: nil)
-  }}
-
+describe PrototypesController, type: :controller do
   context 'user signed in' do
     login_user
+    let(:current_user) { subject.current_user }
 
     describe 'GET #index' do
+      let!(:other_user) { create(:user, name: 'other_user', email: 'other@test.com') }
+      let!(:prototypes) { create_list(:prototype, 2, user: other_user) }
       before { get :index }
 
-      it 'assigns the requested prototypes order by newest' do
-        proto1 = create(:prototype, user_id: user.id, created_at: Date.today)
-        proto2 = create(:prototype, user_id: test_user.id, created_at: Date.tomorrow)
-        expect(assigns(:prototypes)).to eq [proto2, proto1]
-      end
-
-      it 'renders the :index template' do
-        expect(response).to render_template :index
-      end
+      it { expect(assigns(:prototypes)).to eq prototypes }
+      it { expect(response).to render_template :index }
     end
 
     describe 'GET #show' do
-      before { get :show, id: prototype.id }
+      let!(:other_user) { create(:user, name: 'other_user', email: 'other@test.com') }
+      let!(:prototype) { create(:prototype, user: other_user) }
+      before { get :show, params: { id: prototype.id } }
 
-      it 'assigns the requested prototype' do
-        expect(assigns(:prototype)).to eq prototype
-      end
-
-      it 'assigns the user related prototype' do
-        expect(assigns(:user)).to eq test_user
-      end
-
-      it 'renders the :show template' do
-        expect(response).to render_template :show
-      end
+      it { expect(assigns(:prototype)).to eq prototype }
+      it { expect(assigns(:user)).to eq other_user }
+      it { expect(response).to render_template :show }
     end
 
     describe 'GET #new' do
-      it 'renders the :new template' do
-        get :new
-        expect(response).to render_template :new
-      end
+      before { get :new }
+      it { expect(assigns(:prototype)).to be_a(Prototype) }
+      it { expect(response).to render_template :new }
     end
 
     describe 'POST #create' do
-      context 'success to create prototype' do
-        before do
-          post :create, prototype_params
-        end
+      let(:title) { 'タイトル' }
+      let(:catch_copy) { '新しいキャッチコピー' }
+      let(:concept) { '新しいコンセプト' }
+      let(:params) do
+        { params: { prototype: { user: current_user, title: title, catch_copy: catch_copy, concept: concept } } }
+      end
+      before { post :create, params }
 
-        it 'redirects to root_url' do
-          expect(response).to redirect_to root_url
+      context 'when successfully created' do
+        shared_examples_for 'successfully_created' do
+          it 'creates correct attributes' do
+            expect(assigns(:prototype).title).to eq title
+            expect(assigns(:prototype).catch_copy).to eq catch_copy
+            expect(assigns(:prototype).concept).to eq concept
+          end
+          it { expect(response).to redirect_to root_url }
+          it { expect(flash[:notice]).to eq 'プロトタイプを投稿しました。' }
         end
-
-        it 'displays the flash message' do
-          expect(flash[:notice]).to eq 'プロトタイプを投稿しました。'
+        it_behaves_like 'successfully_created'
+        context 'when blank catch_copy' do
+          let(:catch_copy) { '' }
+          it_behaves_like 'successfully_created'
+        end
+        context 'when blank concept' do
+          let(:concept) { '' }
+          it_behaves_like 'successfully_created'
         end
       end
-
-      context 'fail to create prototype' do
-        before do
-          post :create, invalid_prototype_params
-        end
-
-        it 'renders the :new template' do
-          expect(response).to render_template :new
-        end
-
-        it 'displays the flash message' do
-          expect(flash[:warning]).to eq 'プロトタイプの投稿に失敗しました。'
+      context 'when failed to create' do
+        context 'when blank title' do
+          let(:title) { '' }
+          it { expect(response).to render_template :new }
+          it { expect(flash[:warning]).to eq 'プロトタイプの投稿に失敗しました。' }
         end
       end
     end
 
     describe 'GET #edit' do
-      before { get :edit, id: prototype.id }
-
-      it 'assigns the requested prototype' do
-        expect(assigns(:prototype)).to eq prototype
-      end
+      let!(:prototype) { create(:prototype, user: current_user) }
+      before { get :edit, params: { id: prototype.id } }
+      it { expect(assigns(:prototype)).to eq prototype }
+      it { expect(response).to render_template :edit }
     end
 
     describe 'PATCH #update' do
-      context 'success to update prototype' do
-        before do
-          patch :update, prototype_params
-        end
+      let!(:prototype) { create(:prototype, user: current_user) }
+      let(:new_title) { '新しいタイトル' }
+      let(:new_catch_copy) { '新しいキャッチコピー' }
+      let(:new_concept) { '新しいコンセプト' }
+      let(:params) do
+        { params: { id: prototype.id, prototype: { user: current_user, title: new_title, catch_copy: new_catch_copy, concept: new_concept } } }
+      end
+      before { patch :update, params }
 
-        it 'assigns the requested prototype' do
-          expect(assigns(:prototype)).to eq prototype
+      context 'when successfully updated' do
+        shared_examples_for 'successfully_updated' do
+          it 'updates to new_attributes' do
+            prototype.reload
+            expect(prototype.title).to eq new_title
+            expect(prototype.catch_copy).to eq new_catch_copy
+            expect(prototype.concept).to eq new_concept
+          end
+          it { expect(assigns(:prototype)).to eq prototype }
+          it { expect(response).to redirect_to root_url }
+          it { expect(flash[:notice]).to eq 'プロトタイプを更新しました。' }
         end
-
-        it 'redirects to root_url' do
-          expect(response).to redirect_to root_url
+        it_behaves_like 'successfully_updated'
+        context 'when blank catch_copy' do
+          let(:new_catch_copy) { '' }
+          it_behaves_like 'successfully_updated'
         end
-
-        it 'displays the flash message' do
-          expect(flash[:notice]).to eq 'プロトタイプを更新しました。'
+        context 'when blank concept' do
+          let(:new_concept) { '' }
+          it_behaves_like 'successfully_updated'
         end
       end
-
-      context 'fail to update prototype' do
-        before do
-          patch :update, invalid_prototype_params
-        end
-
-        it 'assigns the requested prototype' do
-          expect(assigns(:prototype)).to eq prototype
-        end
-
-        it 'renders the :edit template' do
-          expect(response).to render_template :edit
-        end
-
-        it 'displays the flash message' do
-          expect(flash[:warning]).to eq 'プロトタイプの更新に失敗しました。'
+      context 'when failed to update' do
+        context 'when blank title' do
+          let(:new_title) { '' }
+          it { expect(response).to render_template :edit }
+          it { expect(flash[:warning]).to eq 'プロトタイプの更新に失敗しました。' }
         end
       end
     end
 
     describe 'DELETE #destroy' do
-      before { delete :destroy, id: prototype.id }
-
-      it 'redirects to root_url' do
-        expect(response).to redirect_to root_url
-      end
-
-      it 'displays the flash message' do
-        expect(flash[:notice]).to eq 'プロトタイプを削除しました。'
-      end
+      let!(:prototype) { create(:prototype, user: current_user) }
+      before { delete :destroy, params: { id: prototype.id } }
+      it { expect(assigns(:prototype)).to eq prototype }
+      it { expect(response).to redirect_to root_url }
+      it { expect(flash[:notice]).to eq 'プロトタイプを削除しました。' }
     end
   end
 
-  context "user didn't signed in" do
+  context 'when user is not signed in' do
+    describe 'GET #index' do
+      let!(:other_user) { create(:user, name: 'other_user', email: 'other@test.com') }
+      let!(:prototypes) { create_list(:prototype, 2, user: other_user) }
+      before { get :index }
+
+      it { expect(assigns(:prototypes)).to eq prototypes }
+      it { expect(response).to render_template :index }
+    end
     describe 'POST #create' do
-      it 'redirects to sign_in page' do
-        post :create, prototype_params
-        expect(response).to redirect_to new_user_session_path
-      end
+      before { post :create }
+      it {expect(response).to redirect_to new_user_session_path }
     end
-
     describe 'GET #edit' do
-      it 'redirects to sign_in page' do
-        get :edit, id: prototype.id
-        expect(response).to redirect_to new_user_session_path
-      end
+      let!(:other_user) { create(:user, name: 'other_user', email: 'other@test.com') }
+      let!(:prototype) { create(:prototype, user: other_user) }
+      before{ get :edit, params: { id: prototype.id } }
+      it { expect(response).to redirect_to new_user_session_path }
     end
-
     describe 'PATCH #update' do
-      it 'redirects to sign_in page' do
-        patch :update, prototype_params
-        expect(response).to redirect_to new_user_session_path
-      end
+      let!(:other_user) { create(:user, name: 'other_user', email: 'other@test.com') }
+      let!(:prototype) { create(:prototype, user: other_user) }
+      before { patch :update, params: { id: prototype.id } }
+      it { expect(response).to redirect_to new_user_session_path }
     end
-
     describe 'DELETE #destroy' do
-      it 'redirects to sign_in page' do
-        delete :destroy, id: prototype.id
-        expect(response).to redirect_to new_user_session_path
-      end
+      let!(:other_user) { create(:user, name: 'other_user', email: 'other@test.com') }
+      let!(:prototype) { create(:prototype, user: other_user) }
+      before { delete :destroy, params: { id: prototype.id } }
+      it { expect(response).to redirect_to new_user_session_path }
     end
   end
 end
